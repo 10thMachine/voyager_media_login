4b6ab27b59e9aa7d2f221bf1f602e5a3107488a7
voyagertestdev <voyagermediadev@protonmail.com>
2019-07-29 15:32:30 -0700

    Add AWS Cognito

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // NPM Packages to make the app function
-const express 	= require('express');
-const app		= express();
+const express 		= require('express');
+const app			= express();
+const bodyParser 	= require('body-parser');
+
+// Load environment variables
+const dotenv 	= require('dotenv');
+dotenv.config();
+
+const PORT 		= process.env.PORT;
 
 // AWS Cognito Package
 // const AmzonCognitoIdentity	= require('amazon-cognito-identity-js');
+global.fetch 	= require('node-fetch');
 
-//view engine
-// const ejs		= require('ejs');
-// app.set('view engine', 'ejs');
+// view engine
+const ejs		= require('ejs');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes for Login
 app.use('/', require('./routes/landing.js'));
 
-app.listen(3000, () => {
-	console.log('Server is up');
+app.listen(PORT, () => {
+	console.log(`Server is up on port ${PORT}`);
 })
--- routes/landing.js
+++ routes/landing.js
@@ -2,9 +2,48 @@
 const express 	= require('express');
 const router	= express.Router();
 
+const bodyParser = require('body-parser');
+
+// AWS Cognito Package
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+// AWS Cognito Pool
+const poolData = {
+	UserPoolId : process.env.AWS_UserPoolId,
+	ClientId : process.env.AWS_ClientId
+}
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
 // Routes
 router.get('/', (req, res) => {
-	res.send('Inital route');
-})
+	res.render('register');
+});
+
+router.post('/', (req, res) => {
+	// Item to get the email info
+	const email = req.body.email;
+
+	// Password for the user
+	const password = req.body.password;
+
+	// Email Data for AWS Cognito
+	const emailData = {
+		Name: 'email',
+		Value: email
+	};
+
+	// Email attribute for AWS Cognito
+	const emailAttribute = new AmazonCognitoIdentity.CognitoUserAttribute(emailData);
+
+	userPool.signUp(email, password, [emailAttribute], null, (err, data) => {
+		if(err){
+			return console.error(err);
+		} else {
+			console.log('email has been added');
+			res.send('register completed');
+		}
+	});
+});
 
 module.exports = router;
